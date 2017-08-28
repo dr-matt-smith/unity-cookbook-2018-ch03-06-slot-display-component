eef851cd543adb4ef4280b06f3e37870896c7962
dr-matt-smith <dr_matt_smith@me.com>
2017-08-28 09:48:35 -0400

    added files to project

---
+++ Assets/_Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerInventory))]
+public class PlayerInventoryDisplay : MonoBehaviour 
+{
+	// default to size 1 - but user can overried in Inspector at Design-Time
+	public PickupUI[] slots = new PickupUI[1];
+	
+	public void OnChangeStarTotal(int starTotal)
+	{
+		int numInventorySlots = slots.Length;
+		for(int i = 0; i < numInventorySlots; i++){
+			PickupUI slot = slots[i];
+			if(i < starTotal)
+				slot.DisplayColorIcon();
+			else
+				slot.DisplayGreyIcon();
+		}
+	}
+}
---
+++ Assets/_Scripts/scene2_scripts/PlayerInventoryDisplayKeys.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerInventoryKeys))]
+public class PlayerInventoryDisplayKeys : MonoBehaviour 
+{
+	// default to size 1 - but user can overried in Inspector at Design-Time
+	public PickupUI[] slotsStars = new PickupUI[1];
+
+	// default to size 1 - but user can overried in Inspector at Design-Time
+	public PickupUI[] slotsKeys = new PickupUI[1];
+
+	public void OnChangeStarTotal(int starTotal)
+	{
+		int numInventorySlots = slotsStars.Length;
+		for(int i = 0; i < numInventorySlots; i++){
+			PickupUI slot = slotsStars[i];
+			if(i < starTotal)
+				slot.DisplayColorIcon();
+			else
+				slot.DisplayGreyIcon();
+		}
+	}
+
+	public void OnChangeKeyTotal(int keyTotal)
+	{
+		int numInventorySlots = slotsKeys.Length;
+		for(int i = 0; i < numInventorySlots; i++){
+			PickupUI slot = slotsKeys[i];
+			if(i < keyTotal)
+				slot.DisplayColorIcon();
+			else
+				slot.DisplayGreyIcon();
+		}
+	}
+}
